1945457fc67c96cccb9c574e8e702debf5589a1a
MDM <mdm.pro4@gmail.com>
2026-08-25 22:10:13 -0300

    feat: Add Header and Transactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
- export function App () {
-  return(
-    <div>Hello Word</div>
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+        <Transactions/>
+    </ThemeProvider>
   )
- }
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components"
+import logoImg from "../assets/MF.svg"
+
+const HeaderContainer = styled.header`
+background: ${props => props.theme["gray-900"]};
+padding: 2.5rem;
+`
+
+const HeaderContent = styled.div`
+width: 100%;
+max-width: 1120px;
+margin: 0;
+padding: 0 1.5rem;
+
+display: flex;
+justify-content: space-between;
+align-items: center;
+`
+
+const NewTransactionButton = styled.button`
+height: 50px;
+border: 0;
+background: ${props=> props.theme["green-500"]};
+color: ${props=>props.theme["white"]};
+font-weight: bold;
+padding: 0 1.25rem;
+border-radius: 6px;
+cursor: pointer;
+transition: background-color 0.2s;
+
+&:hover {
+    background: ${props=> props.theme["green-700"]};
+
+}
+`
+
+
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImg} alt="" />
+                <NewTransactionButton>Nova transação</NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
+
+    )
+}
