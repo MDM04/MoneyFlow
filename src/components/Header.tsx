2e6481856472c960b84294680664b66af0d97c10
MDM <mdm.pro4@gmail.com>
2026-08-28 21:48:14 -0300

    feat: Add Modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import logoImg from "../assets/Logo.png"
+import * as Dialog from "@radix-ui/react-dialog"
 
 export const HeaderContainer = styled.header`
   background: ${props => props.theme["gray-900"]};
@@ -40,13 +41,24 @@ export const NewTransactionButton = styled.button`
 
 
 export function Header() {
-    return (
-        <HeaderContainer>
-            <HeaderContent>
-                <img src={logoImg} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
-            </HeaderContent>
-        </HeaderContainer>
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
 
-    )
+  )
 }
